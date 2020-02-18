1ac16575cab2db3d9bcb2a7741adb4da9bcff1b9
mahy50 <934037783@qq.com>
2020-02-18 15:31:13 +0800

    添加准备页面的动画

--- pages/challenge/ready.vue
+++ pages/challenge/ready.vue
@@ -1,5 +1,5 @@
 <template>
-	<view class="ready" :style="{'height': `calc(100vh - ${customBar})`}">
+	<view class="ready" :style="{ height: `calc(100vh - ${customBar})` }">
 		<view class="title">准备就绪！</view>
 		<view class="left-side left">
 			<image class="name-bg left" src="../../static/images/ready-left-bg.png"></image>
@@ -15,131 +15,169 @@
 			<view class="nickname">{{ nickname }}</view>
 			<view class="victory">{{ 0 }}连胜</view>
 		</view>
-		<view class="vs-image">
-			<image src="../../static/images/vs.png"></image>
-		</view>
+		<view class="vs-image"><image src="../../static/images/vs.png"></image></view>
 	</view>
 </template>
 
 <script>
-	export default {
-		computed: {
-			nickname: () => getApp().globalData.nickname,
-			avatar: () => getApp().globalData.avatar,
-			victory: () => getApp().globalData.victory
-		},
-		data() {
-			return {
-				customBar: this.CustomBar
-			}
-		}
+export default {
+	computed: {
+		nickname: () => getApp().globalData.nickname,
+		avatar: () => getApp().globalData.avatar,
+		victory: () => getApp().globalData.victory
+	},
+	data() {
+		return {
+			customBar: this.CustomBar
+		};
 	}
+};
 </script>
 
 <style lang="scss" scoped>
-	.ready {
-		position: relative;
-		padding-top: 180rpx;
-		.title {
-			text-align: center;
-			font-size: 54rpx;
-			font-weight: bold;
-			color: #fff;
-		}
-		.left-side,
-		.right-side {
+.ready {
+	position: relative;
+	padding-top: 180rpx;
+	.title {
+		animation: enlarge 0.3s 0.2s linear forwards;
+		text-align: center;
+		font-size: 54rpx;
+		font-weight: bold;
+		color: #fff;
+		opacity: 0.1;
+	}
+	.left-side,
+	.right-side {
+		position: absolute;
+		.victory-bg,
+		.avatar,
+		.nickname,
+		.victory {
 			position: absolute;
-			.victory-bg,
-			.avatar,
-			.nickname,
-			.victory {
-				position: absolute;
-			}
-			.victory-bg {
-				width: 210rpx;
-				height: 186rpx;
-				bottom: 0;
-			}
-			.avatar {
-				border: 8rpx solid #fff;
-				border-radius: 100rpx;
-				overflow: hidden;
-				width: 100rpx;
-				height: 100rpx;
-			}
-			.nickname {
-				font-size: 36rpx;
-				font-weight: 500;
-				color: #fff;
-				transform: rotateZ(30deg);
-			}
-			.victory {
-				font-size: 40rpx;
-				color: #fff;
-				font-weight: 500;
-				transform: rotateZ(30deg);
-				text-shadow:0px 1px 2px rgba(0,0,0,0.5);
-			}
 		}
-		.left-side {
-			top: 160rpx;
-			.name-bg {
-				width: 350rpx;
-				height: 575rpx;
-			}
-			.avatar {
-				top: 99rpx;
-				left: 60rpx;
-			}
-			.nickname {
-				top: 257rpx;
-				left: 54rpx;
-			}
-			.victory {
-				top: 469rpx;
-				left: 50rpx;
-			}
-		}
-		.right-side {
-			top: 560rpx;
-			.name-bg {
-				width: 390rpx;
-				height: 490rpx; // 470
-				padding-bottom: 20rpx;
-			}
-			.avatar {
-				top: 18rpx;
-				right: 60rpx;
-			}
-			.nickname {
-				top: 190rpx;
-				right: 27px;
-			}
-			.victory {
-				top: 376rpx;
-				right: 13px;
-			}
-		}
-		
-		.vs-image {
-			position: absolute;
-			top: 450rpx;
-			left: 50%;
-			transform: translateX(-50%);
-			text-align: center;
-			image {
-				width: 380rpx;
-				height: 380rpx;
-			}
-		}
-		.left {
-			left: 0;
-		}
-		.right {
-			right: 0;
-		}
-		.bottom {
+		.victory-bg {
+			width: 210rpx;
+			height: 186rpx;
 			bottom: 0;
 		}
+		.avatar {
+			border: 8rpx solid #fff;
+			border-radius: 100rpx;
+			overflow: hidden;
+			width: 100rpx;
+			height: 100rpx;
+		}
+		.nickname {
+			font-size: 36rpx;
+			font-weight: 500;
+			color: #fff;
+			transform: rotateZ(30deg);
+		}
+		.victory {
+			font-size: 40rpx;
+			color: #fff;
+			font-weight: 500;
+			transform: rotateZ(30deg);
+			text-shadow: 0px 1px 2px rgba(0, 0, 0, 0.5);
+		}
 	}
+	.left-side {
+		top: 160rpx;
+		animation: fadeInUpLeft 1s 0s linear forwards;
+		.name-bg {
+			width: 350rpx;
+			height: 575rpx;
+		}
+		.avatar {
+			top: 99rpx;
+			left: 60rpx;
+		}
+		.nickname {
+			top: 257rpx;
+			left: 54rpx;
+		}
+		.victory {
+			top: 469rpx;
+			left: 50rpx;
+		}
+	}
+	.right-side {
+		top: 560rpx;
+		animation: fadeInBottomRight 1s 0s linear forwards;
+		.name-bg {
+			width: 390rpx;
+			height: 490rpx; // 470
+			padding-bottom: 20rpx;
+		}
+		.avatar {
+			top: 18rpx;
+			right: 60rpx;
+		}
+		.nickname {
+			top: 190rpx;
+			right: 27px;
+		}
+		.victory {
+			top: 376rpx;
+			right: 13px;
+		}
+	}
+
+	.vs-image {
+		position: absolute;
+		animation: enlarge 0.3s 1.1s linear forwards;
+		top: 450rpx;
+		left: 50%;
+		margin-left: (-180rpx);
+		text-align: center;
+		image {
+			width: 380rpx;
+			height: 380rpx;
+		}
+		opacity: 0;
+	}
+	.left {
+		left: 0;
+	}
+	.right {
+		right: 0;
+	}
+	.bottom {
+		bottom: 0;
+	}
+}
+
+/*从左下到右上*/
+@keyframes fadeInUpLeft {
+	from {
+		opacity: 0;
+		transform: translate(-1000px, -800px);
+	}
+	to {
+		opacity: 1;
+		transform: translate(0, 0);
+	}
+}
+/*从左下到右上*/
+@keyframes fadeInBottomRight {
+	from {
+		opacity: 0;
+		transform: translate(1000px, 800px);
+	}
+	to {
+		opacity: 1;
+		transform: translate(0, 0);
+	}
+}
+/* 放大效果*/
+@keyframes enlarge {
+	from {
+		opacity: 0;
+		transform: scale(0.1, 0.1);
+	}
+	to {
+		opacity: 1;
+		transform: scale(1, 1);
+	}
+}
 </style>
