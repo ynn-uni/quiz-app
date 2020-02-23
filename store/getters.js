b60fe1311529175946b7aaeffbf0ff5f88380581
mahy50 <934037783@qq.com>
2020-02-23 23:18:56 +0800

    update; 添加getters

--- store/getters.js
+++ store/getters.js
@@ -4,5 +4,14 @@ export default {
   },
   opponentInfo: state => {
     return state.challenge.opponentInfo
+  },
+  socketInstance: state => {
+    return state.challenge.socketInstance
+  },
+  userScore: state => {
+    return state.challenge.userScore
+  },
+  opponentScore: state => {
+    return state.challenge.opponentScore
   }
 }
