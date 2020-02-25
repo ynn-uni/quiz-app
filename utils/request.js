a6eccb65639edaa2320cbf1d52128fb30ce4bfad
mahy50 <934037783@qq.com>
2020-02-25 17:14:35 +0800

    update; 更新请求方法，处理返回数据

--- utils/request.js
+++ utils/request.js
@@ -1,15 +1,24 @@
 // TODO store 中存储获取token
-const baseUrl = 'http://47.107.151.209:9801';
+// const baseUrl = 'http://47.107.151.209:9801';
+const baseUrl = 'http://192.168.1.65:9501';
 
 export async function request(url = '', method = 'GET', data = {}) {
-  return uni.request({
-    url: `${baseUrl}${url}`,
-    data,
-    header: {
-      token: ''
-    },
-    method
-  });
+  return uni
+    .request({
+      url: `${baseUrl}${url}`,
+      data,
+      header: {
+        token: ''
+      },
+      method
+    })
+    .then(data => {
+      const [error, res] = data;
+      if (error) {
+        console.log(error);
+      }
+      return res.data;
+    });
 }
 
 export async function postAction(url, data) {
