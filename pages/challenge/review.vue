fa5c0c510172f45475c2ea97ea8a782e0545925e
mahy50 <934037783@qq.com>
2020-02-27 14:21:03 +0800

    add; 添加review组件

---
+++ pages/challenge/review.vue
@@ -0,0 +1,27 @@
+<template>
+  <view class="bg-linear">
+    <user-pannel ref="pannel" />
+    <subject ref="subject" type="review" :list="questionList" @finish="handleFinish"></subject>
+  </view>
+</template>
+
+<script>
+import UserPannel from '../../components/user-pannel';
+import Subject from '../../components/subject';
+import { mapGetters } from 'vuex';
+export default {
+  name: 'Review',
+  components: { UserPannel, Subject },
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters(['questionList'])
+  },
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
