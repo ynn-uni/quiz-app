2787c56c393ba05d7ea4a5242daae41869bd63fc
mahy50 <934037783@qq.com>
2020-02-29 12:00:49 +0800

    update; 更新练习答题样式

--- pages/exercise/exercise.vue
+++ pages/exercise/exercise.vue
@@ -5,38 +5,57 @@
       <block slot="content">竞猜答题</block>
     </cu-custom>
     <user-pannel ref="pannel" :content="content" />
-    <subject
+    <view
       v-if="questionList.length"
-      type="exercise"
-      :list="questionList"
+      class="serial-number"
+    >{{ curIndex + 1 }} / {{ questionList.length }}</view>
+    <question
+      v-if="questionList.length"
+      ref="question"
+      :question="curQuestion"
+      :disabled="disabled"
       @select="handleSelect"
-      @finish="handleFinished"
     />
+    <view class="action-wrap" v-if="showNext">
+      <button
+        v-if="curIndex < questionList.length - 1"
+        class="action-btn cu-btn round bg-orange text-white"
+        @click="handleNext"
+      >下一题</button>
+      <button
+        v-else
+        class="action-btn cu-btn round bg-orange text-white"
+        @click="handlerFinish"
+      >完成练习</button>
+    </view>
   </view>
 </template>
 
 <script>
 import UserPannel from '../../components/user-pannel.vue';
 import Subject from '../../components/subject.vue';
+import Question from '../../components/Question.vue';
 import { getQuestions } from '../../apis/index';
 import { mapGetters } from 'vuex';
 export default {
-  components: { UserPannel, Subject },
+  components: { UserPannel, Question },
   data() {
     return {
       questionList: null,
+      curIndex: 0,
+      showNext: false,
       answerList: []
     };
   },
   computed: {
     ...mapGetters(['userInfo']),
-    wrongCount() {
-      return this.answerList.filter(i => !i).length;
+    curQuestion() {
+      return this.questionList && this.questionList[this.curIndex];
     },
     content() {
       let correctRate = 100;
       const length = this.answerList.length;
-      const rightCount = this.answerList.filter(i => i).length;
+      const rightCount = this.answerList.filter(i => i.isRight).length;
       if (length) {
         correctRate = rightCount / length * 100;
       }
@@ -53,9 +72,15 @@ export default {
       });
     },
     handleSelect(evt) {
+      this.showNext = true;
       this.answerList.push(evt);
     },
-    handleFinished(evt) {
+    handleNext() {
+      this.curIndex++;
+      this.showNext = false;
+      this.$refs.question.resetResult();
+    },
+    handlerFinish(evt) {
       uni.showModal({
         title: '恭喜！',
         content: `完成练习，${this.content}`,
@@ -78,4 +103,22 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.action-wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  margin-top: 20rpx;
+  .action-btn {
+    margin: 30rpx 0;
+  }
+  .cu-btn {
+    position: relative;
+    padding: 24rpx;
+    width: 50%;
+    font-size: 32rpx;
+    box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.5);
+  }
+}
+</style>
