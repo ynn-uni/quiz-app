3b26a4642efb48bb21a9787b2d92909c459a672b
mahy50 <934037783@qq.com>
2020-02-29 11:54:55 +0800

    add; 添加cover的跳转按钮, 避免卡死时无法运行

--- pages/cover/cover.vue
+++ pages/cover/cover.vue
@@ -1,6 +1,7 @@
 <template>
   <view class="cover-page bg-linear">
     <image class="cover" src="../../static/images/cover.gif" />
+    <button v-if="showBtn" class="cu-btn setting" @click="turnToIndex">进入首页</button>
     <!-- <button class="setting" open-type="openSetting">授权设置</button> -->
   </view>
 </template>
@@ -8,12 +9,25 @@
 <script>
 export default {
   name: 'Cover',
+  data() {
+    return {
+      showBtn: false
+    };
+  },
   onLoad() {
     setTimeout(() => {
+      this.turnToIndex();
+    }, 3000);
+    setTimeout(() => {
+      this.showBtn = true;
+    }, 10000);
+  },
+  methods: {
+    turnToIndex() {
       uni.navigateTo({
         url: '/pages/index/index'
       });
-    }, 3000);
+    }
   }
 };
 </script>
